8d7adc4de80d4eb1c85327dfba419e074b6bdfe6
Влад Михайличенко <mykhailichenkovlad@gmail.com>
2023-03-20 01:59:31 +0200

    init project

---
+++ Address.cs
@@ -0,0 +1,44 @@
+﻿using ExceptionLibrary;
+using System;
+
+namespace HW_14_03_23_exceptions
+{
+    public class Address
+    {
+        private string city;
+        private string street;
+        private string homeNumber;
+
+        public Address(string city, string street, string homeNumber)
+        {
+            setCity(city);
+            setStreet(street);
+            setHomeNumber(homeNumber);
+        }
+
+        public void setCity(string city)
+        {
+            if (String.IsNullOrEmpty(city) || String.IsNullOrWhiteSpace(city)) throw new StringException();
+            this.city = city;
+        }
+        public void setStreet(string street)
+        {
+            if (String.IsNullOrEmpty(street) || String.IsNullOrWhiteSpace(street)) throw new StringException();
+            this.street = street;
+        }
+        public void setHomeNumber(string homeNumber)
+        {
+            if (String.IsNullOrEmpty(homeNumber) || String.IsNullOrWhiteSpace(homeNumber)) throw new StringException();
+            this.homeNumber = homeNumber;
+        }
+
+        public string getCity() { return this.city; }
+        public string getStreet() { return this.street; }
+        public string getHomeNumber() { return this.homeNumber; }
+
+        public override string ToString()
+        {
+            return ($"{getCity()}, {getStreet()} {getHomeNumber()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using HW_14_03_23_exceptions;
+
+namespace HW_16_03_23_operator_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Student st = new Student();
+
+			try
+			{
+				st.setSurname("");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+        }
+    }
+}
